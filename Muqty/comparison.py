0af4e704f8e676cfc12c6ef33bd4fd24909b70a8
Muqtar <tolafejay@gmail.com>
2021-08-17 20:22:33 +0100

    first commit

---
+++ Muqty/comparison.py
@@ -0,0 +1,14 @@
+first_number = int(input("enter first number"))
+second_number = int(input("enter second number"))
+third_number = int(input("enter third number"))
+
+first_number_int = int(first_number)
+second_number_int = int(second_number)
+third_number_int = int(third_number)
+
+
+if first_number < second_number < third_number:
+    print(True)
+else:
+    print(False)
+
