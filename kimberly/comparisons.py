10a326087fe2ba9e49655d74cccd2cce59b1d9c3
Mojoyinola Oketope <m.oketope@semicolon.africa>
2021-08-17 16:00:37 +0100

    added comparison file

---
+++ kimberly/comparisons.py
@@ -0,0 +1,9 @@
+first_number = int(input("enter first number: "))
+second_number= int(input("enter second number: "))
+third_number=int(input("enter third number: "))
+
+
+if first_number < second_number < third_number:
+  print(True)
+else :
+  print(False)
