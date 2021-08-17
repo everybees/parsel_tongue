b0178728ae82ccff7772ded2a1a414ddb6d490af
Sweetlight95 <princessadesuwa93@gmail.com>
2021-08-17 16:16:56 +0100

    add new maths

--- adesuwa/adesuwa_timi.py
+++ adesuwa/adesuwa_timi.py
@@ -1,3 +1,11 @@
 print("Adesuwa")
 print("Python is great to learn but you need to give it your best attention, if not it will leave the person")
 print("lets keep pushing it and praying towards learning")
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enetr second number: "))
+third_number = int(input("Enter third number: "))
+if first_number < second_number < third_number:
+        print(True)
+else:
+    print(False)
