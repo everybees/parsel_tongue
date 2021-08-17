ebff4b85dad4ed2225914a172c70906308e2ba33
*Emmanuel <*emmanuelolusegun.co@gmail.com*>
2021-08-18 00:29:10 +0100

    Deleted unwanted 8folder/file and updated 2Folders/Files

--- comparisons/comparisons.py
+++ comparisons/comparisons.py
@@ -1,13 +1,39 @@
 
+
+
+# Class Work one
 firstNumber = int(input("supply firstNumber: "))
 secondNumber = int(input("supply the second number: "))
-thirdNumber = int(input("supply the third number: "));
-
+thirdNumber = int(input("supply the third number: "))
 
 # if thirdNumber > secondNumber > firstNumber:
 #     print(True)
 # else:
 #     print(False)
-
-
 print(thirdNumber > secondNumber > firstNumber)
+
+
+# Exercise: Class Work two
+
+# strip is meant to remove the space b4 an after
+a = int(input("supply a number: ").strip())
+print(a > 0)
+
+
+
+
+
+print(False and False);
+print(True and False)
+print(False or False)
+print(False or True)
+print(True and True)
+print(True or True)
+
+patient_full_name = "John Smith"
+patient_age = 20
+is_patient_new = True;
+print(patient_full_name, patient_age, is_patient_new)
+
+
+# print and input are function built into Python Library
