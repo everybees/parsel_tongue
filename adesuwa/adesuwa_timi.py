719d3c391f83c216d9b7c132ae874e3ea4b99b65
Sweetlight95 <princessadesuwa93@gmail.com>
2021-08-17 14:35:56 +0100

    Adding a new file again

---
+++ adesuwa/adesuwa_timi.py
@@ -0,0 +1,3 @@
+print("Adesuwa")
+print("Python is great to learn but you need to give it your best attention, if not it will leave the person")
+print("lets keep pushing it and praying towards learning")
