c11342d96eecbd6e1e83d4ad52fa3a2578ff647a
Ozioma <oziomaokoroafor@gmail.com>
2021-08-18 15:13:53 +0100

    Number size checker

--- ozioma/else_statement.py
+++ ozioma/else_statement.py
@@ -4,14 +4,31 @@
 # Display "Leap year" or "Ordinary"
 #
 
-year_input = int(input("Enter year: "))
-
-
 def leap_year_calculator():
+    year_input = int(input("Enter year: "))
+
     if year_input % 4 == 0 and year_input % 100 != 0 or year_input % 400 == 0:
         print("Leap year")
     else:
         print("Ordinary")
 
 
-leap_year_calculator()
+#leap_year_calculator()
+
+
+# write two numbers
+# show the biggest number on the first line and smallest number on the second line
+
+def number_size_checker():
+    first_number = int(input())
+    second_number = int(input())
+    biggest_number = first_number
+    if second_number > first_number:
+        biggest_number = second_number
+        print(biggest_number)
+        print(first_number)
+    else:
+        print(first_number)
+        print(second_number)
+
+number_size_checker()
