267af32d8bcf014c554791979aebb85f000d5910
forverchild <dabreezy8@gmail.com>
2021-08-18 16:45:28 +0100

    class work

--- foreverchild/second_file.py
+++ foreverchild/second_file.py
@@ -1,3 +1,10 @@
-print("foreverchild")
-print("Python Class")
-print("This python class how you see am my people!")
+n  = int (input())
+if n %2 == 0:
+    print("Weird")
+if n % 2 == 0 and 2 >= n <= 5:
+    print("Not weird")
+if n % 2 == 0 and 6 >= n <= 20:
+    print("Weird")
+if n % 2 == 0 and n > 20:
+    print("Not weird")
+
