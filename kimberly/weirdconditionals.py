3d84bba3c6974a9f147ff982f7adee9dd0f65d02
Mojoyinola Oketope <m.oketope@semicolon.africa>
2021-08-18 22:18:35 +0100

    added modified weirdconditionals file

--- kimberly/weirdconditionals.py
+++ kimberly/weirdconditionals.py
@@ -4,7 +4,7 @@ if n % 2 != 0:
 if n % 2 == 0 and 2 <= n <=5:
     print("Not weird")
 if n % 2 == 0 and 6 <= n <=20:
-    print("Weird")
+   print("Weird")
 if n % 2 == 0 and n > 20:
-    print("Not weird")
+   print("Not weird")
                
