b7a1d4d77bcf38f9c20b42bde143a9305b810907
Emmanuel <jospag4christ@yahoo.com>
2021-08-18 17:09:04 +0100

    Added class exercises

--- Emmanuel/else_statement.py
+++ Emmanuel/else_statement.py
@@ -3,4 +3,17 @@ year = int(input())
 if (year % 4 == 0 and year % 100 != 0) or year % 400 ==0:
     print("Leap")
 else:
-    print("Ordinary")    
+    print("Ordinary")  
+
+
+first = int(input())
+second = int(input("\n"))
+
+maximum = first
+
+if second > maximum:
+    print("\n",+second)
+    print("\n",+first)
+else:
+    print(first,"\n")
+    print(second,"\n")
