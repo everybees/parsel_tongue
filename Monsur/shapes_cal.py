bbdc0b45fc8818a8ce4dbbc89fce645af3226f91
Monsur Kareem <monsurkareem@gmail.com>
2021-08-18 12:47:51 +0100

    deleted some files

--- Monsur/shapes_cal.py
+++ Monsur/shapes_cal.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 radius = float(input("Enter your radius: "))
 pi = 3.14
