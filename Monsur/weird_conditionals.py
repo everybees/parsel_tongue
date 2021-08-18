a77c4d82510e5f7c71adc7beb1780b99b632179f
Monsur Kareem <monsurkareem@gmail.com>
2021-08-18 17:06:11 +0100

    amend new class assignment

--- Monsur/weird_conditionals.py
+++ Monsur/weird_conditionals.py
@@ -1,8 +1,8 @@
 N = int(input("Enter number: "))
 
-if N % 2 == 0:
+if N % 2 != 0:
     print("Weird")
-if N % 2 == 0 and 2 >= N <= 5:
+if N % 2 == 0 and 2 <= N <= 5:
     print("Not Weird")
 if N % 2 == 0 and 6 <= N <= 20:
     print("Weird")
