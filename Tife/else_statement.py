cfb37567c0b44c9b29df4db70585134e359a61fd
tifeolanipekun <tifeolanipekun@gmail.com>
2021-08-24 09:15:04 +0100

    Assignment added

---
+++ Tife/else_statement.py
@@ -0,0 +1,10 @@
+# Leap year checker
+year = int(input ("Enter a year: "))
+print("It is a leap year" if (year % 4 == 0 and year % 100 != 0) else "Ordinary")
+
+# number_one = int(input("Enter a number: "))
+# number_two = int(input("Enter another number: "))
+# if number_one > number_two :
+#     print(number_one,"\n",number_two)
+# if number_two >  number_one  :
+#     print(number_two,"\n",number_one) 
