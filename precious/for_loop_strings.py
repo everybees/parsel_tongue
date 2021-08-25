7e729bc37d64814b9d25ae511b5f6e551d10e53d
Lois <onyeukwuamarachi@gmail.com>
2021-08-25 09:38:28 +0100

    added my assignment

---
+++ precious/for_loop_strings.py
@@ -0,0 +1,4 @@
+string_number = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+string = "vowel"
+for string in string_number:
+    print(string, string_number)
