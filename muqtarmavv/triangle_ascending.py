28996f0369e884a2ad0db97deaeda46e1e8f3ea5
Muqtar <88159236+Muqtarmav@users.noreply.github.com>
2021-08-28 20:58:16 +0100

    Delete muqtarmavv directory

--- muqtarmavv/triangle_ascending.py
+++
@@ -1,8 +0,0 @@
-rows = int(input("enter number of rows: "))
-for numbers in range(1, rows+1):
-     for columns in range(1, numbers+1):
-         print(columns, end="")
-     print("")
-
-
-
--- muqtarmavv/triangle_descending1.py
+++
@@ -1,5 +0,0 @@
-rows = int(input("enter number of rows: "))
-for i in range(rows, 0, - 1):
-    for columns in range(i, 0, -1):
-        print(columns, end="")
-    print("")
