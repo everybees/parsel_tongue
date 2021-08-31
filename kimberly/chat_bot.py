baf133de9cb2b2d3edf1f4f4b6b4ecd059a1b980
Mojoyinola Oketope <m.oketope@semicolon.africa>
2021-08-31 15:29:09 +0100

    added new chatbot file

---
+++ kimberly/chat_bot.py
@@ -0,0 +1,47 @@
+import datetime
+import random
+
+print("This is Kim. How can I help you today?")
+question = input()
+
+love = ["Yes, I love you!", "No, I don't love you", "What is love?", "Love is wicked!"]
+age = [num for num in range(1,100)]
+eat = ["No, I haven't.", "Yeah, I have had breakfast.", "No, I am fasting", "Yeah, I had pizza and iecream."]
+relationships= ["Wow, even the gods do not know.", "Yes, I am single.", "Yes, I am in a relationship", "It is complicated."]
+home= ["Yes i am.", "No, i'm not" ]
+visit = ["Don't even dare","Disembark from that nonsense capping", "It would be a pleasure"]
+music = ["hiphop is life!","rap is king!", "pop is my fav", "afrobeats for life"]
+dance = ["Lmao, I can't dance to save my life.", "I started dancing in my mother's womb.", "Dancing is my hobby", "I don't dance."]
+sleep = ["I am,I really need to go to bed now", "Yes, I am, Goodnight!", "No, I'm not", "Yes,i'll talk to you tomorrow"]
+
+
+
+while True:
+    question = input()
+    if "time" in question:
+        print("The time of day is:", datetime.datetime.now().time())
+    elif "name" in question:
+        print("My name is precious, How can i help you?")   
+    elif "old" in question or "age" in question:
+        print("I am", random.choice(age), "years old.")
+    elif "eat" in question or "eaten" in question:
+        print(random.choice(eat))
+    elif "love" in question:
+        print(random.choice(love))
+    elif "single" in question or "relationship" in question:
+        print(random.choice(relationships))
+    elif "home" in question or "alone" in question:
+        print(random.choice(home))
+    elif "visit" in question or "see" in question:
+        print(random.choice(visit)) 
+    elif "music" in question or "song" in question:
+        print(random.choice(music)) 
+    elif "dance" in question or "twerk" in question:
+        print(random.choice(dance))
+    elif "sleep" in question or "tired" in question:
+        print(random.choice(sleep))                 
+    elif "break" in question:
+        print("Seems you want to break up, Boy Bye!")
+        break  
+    else:
+        print("I don't understand you. Do you mind asking another question?")                    
