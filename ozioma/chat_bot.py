14efe95d586b30929cc43b72d660a40666deaba8
Ozioma <oziomaokoroafor@gmail.com>
2021-08-31 14:24:54 +0100

    Chat bot commit refactored

--- ozioma/chat_bot.py
+++ ozioma/chat_bot.py
@@ -1,12 +1,16 @@
+import random
 import datetime
 question = input('Ask a question:  ')  # .split()
 # print(question)
+love =["Yes, I love you", "No, I don't love you", "What is love?", "Love is wicked"]
 if "what" and "time" in question:
     print("The time of the day is:", datetime.datetime.now().time())
     if "your name" in question:
         print("My name is Precious!")
 elif "your name" in question:
     print("My name is Precious!")
+elif "love" in question:
+    print(random.choice(love))
 else:
     print("I don't understand you.\nDo you mean What is the time of the day?")
     response = input()
