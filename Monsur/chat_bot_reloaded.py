514e8bd0deb983f21c8d602d09c5a68e4f0c3c1d
Monsur Kareem <monsurkareem@gmail.com>
2021-09-01 10:04:00 +0100

    new class assignment

--- Monsur/chat_bot_reloaded.py
+++ Monsur/chat_bot_reloaded.py
@@ -3,16 +3,16 @@ import datetime
 
 dictionary = {
     "love":  ["yes, I love you!", "No, I don't love you!", "Nah!", "Love is wicked"],
-    "age": ["number for number in range(18, 50)"],
+    "age": [number for number in range(18, 50)],
     "country": ["France", "UK", "Nigeria"],
-    "siblings": ["number for number in range(1, 6)"],
+    "siblings": [number for number in range(1, 6)],
     "crazy": ["A lil", "Not at all", "Sometimes"],
     "relationships": ["Wow, even the gods do not know", "Yes, I am single", "It is complicated"]
     }
 while True:
     user = input("My name is Solibot, how can i help you?: ").split()
-    for keys, value in dictionary.item():
-        if user in keys:
+    for keys, value in dictionary.items():
+        if keys in user:
             print(random.choice(value))
 
 
