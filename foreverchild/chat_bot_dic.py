4a150d13558be552f7ab0b73621803f87094c08f
forverchild <dabreezy8@gmail.com>
2021-09-01 09:55:43 +0100

    modified

--- foreverchild/chat_bot_dic.py
+++ foreverchild/chat_bot_dic.py
@@ -8,7 +8,7 @@ Dict = {"love": ["yes", " i love you", " no, I dont love you", "what is love", "
  "eat": ["No, I haven't", "Yeah, I have had breakfast", "Yeah, I had eba and egusi", "No, I am fasting"], 
  "age": ["No, I haven't", "Yeah, I have had breakfast", "Yeah, I had eba and egusi", "No, I am fasting"], 
  "relationship": ["Wow, even the gods do not know", "Yes, i am single", "Yes, i am in a relationship", "its complicated"], 
- "feeling": ["Wow, even the gods do not know", "Yes, i am single", "Yes, i am in a relationship", "its complicated"], 
+ "feeling": ["I am feeling great today?", "i do not fall sick", "yes, a feeling of joy", "I am not happy",], 
  "smart": ["Yes, i am smarter", "No am not as smart as you think", "I choose to be smart", "its better to be smart than to be dull"], 
  "rain": ["No its not gonna rain today", "I think i might rain", "yes just a bit", "no its sunny today"], 
  "come": ["yes I will be coming around", "Why do u need me arround?", "I will come", "No, Am busy"], 
