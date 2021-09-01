bd508bfd09cdb463908a0df4b6de1d955914bf39
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2021-09-01 11:19:27 +0100

    chat bot version 2

---
+++ tosin/chat_bot_two.py
@@ -0,0 +1,23 @@
+import datetime
+import random
+response = {
+    'love' : ["Yes, I love you", "No, I don't love you", "What is love", "Love is wicked"],
+    'age' : [num for num in range(1,100)],
+    'relationships' : ["Wow, even the gods do not know.", "Yes, I'm single", "Yes, I'm in relationship", "It is complicated"],
+    'eat' : ["I haven't", "Yeah, I have had dinner", "Yeah, I have had my breakfast", "I have eaten"],
+    'weather' : ["It is sunny", "It is cloudy", "snowing", "raining"],
+    'location' : ["Lagos", "Abuja", "Benin", "Ibadan"],
+    'feelings' : ["I'm good", "I'm happy", "I'm tired", "I'm feeling great"],
+    'shopping' : ["Yes", "No"],
+    'days' : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
+}
+print("I'm Muqtar. What question do you want answered today?")
+
+while True:
+    question = input().split()
+    for word in question:
+      if word in response.keys():
+        print(random.choice(response[word]))
+    else:
+        print("You said what? please make me understand")
