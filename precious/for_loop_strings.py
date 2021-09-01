251990cceff1f5f319293ad2242f8b4be8f13e7d
Lois <onyeukwuamarachi@gmail.com>
2021-09-01 01:44:36 +0100

    added my assignment

--- precious/for_loop_strings.py
+++ precious/for_loop_strings.py
@@ -1,4 +1,5 @@
 string_number = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-string = "vowel"
-for string in string_number:
-    print(string, string_number)
+number = input("Enter number: \n ")
+#string = "vowel"
+for digit in number:
+    print(string_number[int(digit)], end=" ")
