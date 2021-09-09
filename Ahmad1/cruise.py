c437a57502bf51814b41ae2fb0b04267209c7225
ajala ahmad <amadajala98@gmail.com>
2021-09-09 16:59:04 +0800

    python files

--- Ahmad1/cruise.py
+++ Ahmad1/cruise.py
@@ -5,19 +5,12 @@ options = {"dish": ["rice", "semo", "amala", "pounded yam", "porridge"],
            "drinks": ["long island", "juice", "wine", "champagne", "chapman"],
            "takeout": ["yes, take out is available", "Sorry, its not allowed"]
            }
-print("Hello, my name is Alexa. how may i help you?")
+print("my name is oladimdim, how can i help you?")
 while True:
     question = input().split()
-    if "dish" in question:
-        print(random.choice(options["dish"]))
-    elif "reservation" in question:
-        print(random.choice(options["reservation"]))
-    elif "drinks" in question:
-        print(random.choice(options["drinks"]))
-    elif "takeout" in question:
-        print(random.choice(options["takeout"]))
-    elif "thanks" in question:
-        print("i'm glad i could be of help. Do enjoy your meal")
-        break
+    for word in question:
+        if word in options.keys():
+            print(random.choice(options[word]))
+            break
     else:
         print("i dont have an answer for that, kindly ask a different question")
