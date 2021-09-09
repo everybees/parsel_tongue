737d3f936191b2146488fecfee619b8711dc36b5
toyinfa2884 <toyinfatai@gmail.com>
2021-09-09 08:56:43 +0100

    assignment

---
+++ fatai/for_loop_print_pattern4.py
@@ -0,0 +1,9 @@
+for i in range(int(input("Enter a number greater than 5:"))):
+    for j in range(i + 1):
+       print("*", end="")
+    print()
+
+for i in range(i):
+    for j in range(j - 1):
+      print("*", end="")
+    print()
---
+++ fatai/integer_number.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number:"))
+if number < 0:
+    print("negative")
+elif number > 0:
+     print("positive")
+elif number == 0:
+    print("zero")
