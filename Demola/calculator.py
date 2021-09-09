7ca451408c72a0ad601e60a56f739e152f9ba5f8
Adeniyi Ademola <Adeniyiademola18@gmail.com>
2021-09-09 04:49:42 -0400

    submitted

---
+++ Demola/calculator.py
@@ -0,0 +1,32 @@
+import operator
+
+first_number = float(input("Enter first Number: "))
+sign = input("Enter a Sign: ")
+second_number = float(input("Enter Second Number: "))
+
+
+
+
+
+operations = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv,
+    '%': operator.mod,
+}
+
+if sign in operations.keys():
+    answer = operations[sign](first_number, second_number)
+    print(answer)
+
+    while True:
+        sign = input("Enter Operator: ")
+        if sign == "exit":
+            print("Calculator Exiting.......")
+            print(answer)
+            break
+        number = float(input("Input Number: "))
+        if sign in operations.keys():
+            answer = operations[sign](answer, number)
+            print(answer)
