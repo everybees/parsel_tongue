7a4304de4dbe1adeb54db96195500fea5fbf64a2
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-10 11:42:00 +0100

    ...

--- Olatoye/new_specials/main.py
+++ Olatoye/new_specials/main.py
@@ -1,9 +1,20 @@
 from Olatoye.new_specials import chatbot, calculator
+from Olatoye.functions import exercise
 
 if __name__ == "__main__":
-    while True:
-        userInput = int(input())
-        if userInput == 1:
-            calculator.calculator()
-        elif userInput == 2:
-            chatbot.chatbot()
+    # userInput = int(input())
+    # if userInput == 1:
+    #     calculator.calculator()
+    # elif userInput == 2:
+    #     chatbot.chatbot()
+
+    # exercise.max_of_three_numbers(4,2,3)
+    # exercise.sum_of_numbers((1,2,2))
+    # exercise.multiple_of_numbers((1,2,2))
+    # exercise.reverse_string("olatoye")
+    # exercise.factorial(4)
+    # exercise.number_in_range(3, 5)
+    # exercise.upper_and_lower("OlaToye")
+    exercise.unique_list_item((1, 1, 3, 4, 5, 2, 1, 2))
+    # exercise.prime_number(7)
+    # exercise.even_number_from((1,2,3,4,5,6))
