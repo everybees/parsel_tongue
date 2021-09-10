3586752a576b91baedef05585db0122d8efa5a16
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-10 14:44:33 +0100

    ...useraccount details...

---
+++ Olatoye/new_specials/user_account_details.py
@@ -0,0 +1,25 @@
+def user_details(counter):
+    user_dictionary = {
+        "id": ["user" + str(counter)],
+        "first_name": [input("Enter first name: ")],
+        "last_name": [input("Enter last name: ")],
+        "email": [input("Enter email address: ")],
+        "password": [input("Enter password: ")]
+    }
+
+    return user_dictionary
+
+
+def enter_user_details():
+    list_of_users = []
+    counter = 1
+    while True:
+        list_of_users.append(user_details(counter))
+        counter += 1
+        if counter == 3:
+            break
+
+    print(list_of_users)
+
+
+enter_user_details()
