0ab9e88a52546e2744382c39850065efeb998f73
forverchild <dabreezy8@gmail.com>
2021-09-16 10:17:33 +0100

    exercises

---
+++ calculator_main.py
@@ -0,0 +1,6 @@
+from foreverchild import calculator
+if __name__ == "__main__":
+    calculator.calculate_main()
+
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from foreverchild import chat_bot
+
+if __name__ == "__main__":
+    chat_bot.do_something()
