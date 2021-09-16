7663146eaab143459044e519393dbbf867c49708
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-16 09:58:23 +0100

    function exercises

--- Olatoye/new_specials/main.py
+++ Olatoye/new_specials/main.py
@@ -6,27 +6,30 @@ def function_caller():
     number = input("Enter your question number: ")
 
     func_dict = {
-        "1": exercise.max_of_three_numbers(),
-        "2": exercise.sum_of_numbers(),
-        "3": exercise.multiple_of_numbers(),
-        "4": exercise.reverse_string(),
-        "5": exercise.factorial(),
-        "6": exercise.number_in_range(),
-        "7": exercise.upper_and_lower(),
-        "8": exercise.unique_list_item(),
-        "9": exercise.prime_number(),
-        "10": exercise.even_number_from(),
-        "11": exercise.perfect_number(),
-        "12": exercise.palindrome(),
-        "15": exercise.hyphen_separator()
+        "1": [exercise.max_of_three_numbers()],
+        "2": [exercise.sum_of_numbers()],
+        "3": [exercise.multiple_of_numbers()],
+        "4": [exercise.reverse_string()],
+        "5": [exercise.factorial()],
+        "6": [exercise.number_in_range()],
+        "7": [exercise.upper_and_lower()],
+        "8": [exercise.unique_list_item()],
+        "9": [exercise.prime_number()],
+        "10": [exercise.even_number_from()],
+        "11": [exercise.perfect_number()],
+        "12": [exercise.palindrome()],
+        "15": [exercise.hyphen_separator()]
     }
 
-    for key, value in func_dict:
-        if number == func_dict.keys():
-            print(f"{key}\n{func_dict.values().__doc__}")
-            print(value)
-        else:
-            print("Sorry, number not in questions list...")
+    for key in func_dict:
+        if key in number:
+            print(func_dict[key])
+    # for key, value in func_dict.items():
+    #     if key == number:
+    #         # print(f"{key}\n{value.__doc__}")
+    #         print(value)
+    #     else:
+    #         print("Sorry, number not in questions list...")
 
 
 if __name__ == "__main__":
