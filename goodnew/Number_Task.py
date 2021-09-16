849d292c7c711359d51704d66a932c61b2519a9c
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-16 17:44:45 +0100

    recently added files

---
+++ goodnew/Number_Task.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number\n"))
+
+if n % 2 == 1:
+    print("weild")
+if n % 2 == 0 and n <= 6 and n >=2:
+    print("Not weild")
+if n % 2 == 0 and n <= 20 and n >=6:
+    print ("Weild")
+if n % 2 == 0 and n > 20:
+    print("Not weild")
