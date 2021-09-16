6c0670f5b1d67c04692e24e0295167fc71d17d15
Sweetlight95 <princessadesuwa93@gmail.com>
2021-09-16 10:10:39 +0100

    Assignment

---
+++ adesuwa/functions/mainfun.py
@@ -0,0 +1,13 @@
+import exercise
+
+if __name__ == "__main__":
+     exercise.max_of_three_numbers()
+    # exercise.sum_of_numbers_in_a_list()
+    # exercise.reverse_a_string()
+    # exercise.multiply_all_list_numbers([2, 6, 8])
+    # exercise.factorial_of_number(5)
+    # exercise.number_in_range(6000)
+    # exercise.numbers_of_lower_and_uppercase("SOLOmanjuie")
+    # exercise.unique_element_list([3, 5, 6, 7, 5, 6, 7])
+    # exercise.even_number([2,3,4,6,7,8,10,11,12,16,14])
+
