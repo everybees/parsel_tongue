2331ec2f01e6311be2b772e5f0d734efcd4b27e4
Ozioma <oziomaokoroafor@gmail.com>
2021-09-16 10:54:43 +0100

    Main file commit

--- ozioma/main.py
+++ ozioma/main.py
@@ -34,12 +34,3 @@ if __name__ == "__main__":
     print('Printing the even numbers in a list')
     print(exercises.print_even_number_from_a_list())
     print('Thank you for partaking in our game.')
-
-    # while True:
-    #     input_number = input('Type 1 to chat, 2 to calculate two numbers or exit to exit the program: ')
-    #     if input_number == '1':
-    #         chat_bot_2.do_somthing()
-    #     elif input_number == '2':
-    #         calculator.calculate()
-    #     elif input_number == 'exit':
-    #         break
