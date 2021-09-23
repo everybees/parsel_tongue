85bfa408de09efd886b17d87d157d18a480b8e77
Ozioma <oziomaokoroafor@gmail.com>
2021-09-23 21:12:43 +0100

    Class work commit

--- ozioma/class_task.py
+++ ozioma/class_task.py
@@ -6,6 +6,13 @@ class Building:
     def __str__(self):
         return self.name + "," + self.cohorts
 
+    def _building_name(self, building_name):
+        if len(building_name) > 20:
+            raise ValueError("Building name is too long.")
+        if type(building_name) is not str:
+            raise ValueError("Input is not applicable here.")
+        return building_name
+
 
 class Cohorts:
     def __init__(self, cohort_name, natives, description):
@@ -16,6 +23,10 @@ class Cohorts:
     def __str__(self):
         return self.cohort_name
 
+    def _check_cohorts(self, cohort):
+        if type(cohort) is not list:
+            raise ValueError("Invalid cohort type.")
+        return cohort
 
 class Native:
     def __init__(self, first_name, last_name, native_id, sex):
@@ -26,3 +37,5 @@ class Native:
 
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+    # def check_native_input(self, native):
