8dd791c5c1782b40a3ca6936cfa3d8686ac98250
Monsur Kareem <monsurkareem@gmail.com>
2021-09-23 11:54:02 +0100

    new class work- edit 1

--- Monsur/class_task/main.py
+++ Monsur/class_task/main.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
     cohorts = []
     native1 = class_task.Native("John", "Eba", "M", "SCN01")
     natives1.append(native1)
+
     native2 = class_task.Native("Good", "News", "M", "SCN02")
     natives1.append(native2)
+
     native3 = class_task.Native("Kim", "Killer", "F", "SCN03")
     native4 = class_task.Native("Leo", "Morris", "M", "SCN04")
     native5 = class_task.Native("Mac", "Manuel", "F", "SCN05")
@@ -41,7 +43,7 @@ if __name__ == "__main__":
             print("SCN  No.|   First name   |   Last name   |   Sex")
             for native in cohort.natives:
                 print(
-                    native.native_id + "        |" + native.first_name + "          |" + native.last_name + "         |" + native.sex)
+                    native.native_id + "        |"+ native.first_name + "          |"+ native.last_name + "         |"+ native.sex)
 
             print(
                 "\n=====================================================================================================")
