34d476b9f38b6fb5e0e1f369da78f6dfc164bcda
Ozioma <oziomaokoroafor@gmail.com>
2021-09-23 22:16:57 +0100

    commit

--- jibola/class_task.py
+++ jibola/class_task.py
@@ -4,10 +4,13 @@ create a class of building
 create a class of cohorts
 create a class of natives
 
-create an object of a building that has 4 cohorts and these cohorts have natives.
+create an object of a building that has 4 cohorts and
+these cohorts have natives.
 
-print the builiding name, the cohorts with their name and description, not forgetting the natives in these
-cohorts with their first name, last name, and their native id(SCN1 and so on).
+print the builiding name, the cohorts with their name and
+description, not forgetting the natives in these
+cohorts with their first name, last name, and their native
+id(SCN1 and so on).
 
 we can have something like this:
 
