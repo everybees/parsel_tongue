3556365cada4e9d18c1bfd7e8dcbf0fa6af08fe9
Ozioma <oziomaokoroafor@gmail.com>
2021-09-25 17:13:07 +0100

    Building commit 2

--- ozioma/class_task.py
+++ ozioma/class_task.py
@@ -24,7 +24,7 @@ class Cohorts:
     def __init__(self, cohort_name, natives, description):
         self.cohort_name = self._check_cohort_name(cohort_name)
         self.cohort_description = description
-        self.natives = natives
+        self.natives = self._check_native(natives)
 
     def __str__(self):
         return self.cohort_name
@@ -36,13 +36,18 @@ class Cohorts:
             raise ValueError("Cohort name too long")
         return cohort_name
 
+    def _check_native(self, native_id):
+        if native_id is not list:
+            raise ValueError("Invalid native input.")
+        return native_id
+
 
 class Native:
-    def __init__(self, first_name, last_name, _native_id, sex):
+    def __init__(self, first_name, last_name, _native_id, gender):
         self.first_name = self._check_first_name(first_name)
-        self.last_name = last_name
+        self.last_name = self._check_last_name(last_name)
         self.native_id = _native_id
-        self.sex = self._check_gender(sex)
+        self.gender = self._check_gender(gender)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
@@ -61,8 +66,13 @@ class Native:
             raise ValueError("Name is longer than 24 characters.")
         return last_name
 
-    def _check_gender(self, sex):
-        new_sex = sex.lower()
-        if new_sex is not "male" and new_sex is not "female":
+    def _check_gender(self, gender):
+        new_gender = gender.lower()
+        if new_gender is not "male" and new_gender is not "female":
             raise ValueError("Invalid input.")
-        return new_sex
+        return new_gender
+
+    def _check_native_id(self, native_id):
+        if native_id is not int:
+            raise ValueError("Invalid input")
+        return native_id
