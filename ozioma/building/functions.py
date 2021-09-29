5e2ff13a2c4d1b4b014d095c2e8a184205d82d15
Ozioma <oziomaokoroafor@gmail.com>
2021-09-29 22:22:51 +0100

    Ozioma assignment commit 2

--- ozioma/building/functions.py
+++ ozioma/building/functions.py
@@ -15,3 +15,32 @@ def create_native(first_name, last_name, native_id, gender):
     native = class_task.Native(first_name, last_name,native_id,gender)
     return native
 
+
+def create_cohort_list():
+    cohorts = []
+    return cohorts
+
+
+def create_native_list():
+    natives = []
+    return natives
+
+
+def add_native_to_list(native):
+    natives = create_native_list()
+    return natives.append(native)
+
+
+def add_cohort_to_list(cohort):
+    cohorts = create_cohort_list()
+    return cohorts.append(cohort)
+
+
+def create_building_list():
+    building = {}
+    return building
+
+
+def add_cohort_to_building(cohort):
+    building = create_building_list()
+    return building[cohort]
