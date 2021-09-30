5ca4296ba7e057a4cef62f3cd459e795e4a18afe
djfemz <oladejifemi00@gmail.com>
2021-09-30 08:35:00 +0100

    refactored

--- femi/main.py
+++ femi/main.py
@@ -1,5 +1,33 @@
 import class_task
 
+
+def create_cohort():
+    number_of_cohorts=int(input("how many cohorts do you want to create?\n"))
+    for _ in range(number_of_cohorts):
+        cohort_name = input("Enter cohort name\n")
+        cohort_desc = input("Enter cohort description\n")
+        new_cohort = class_task.Cohort(cohort_name, cohort_desc)
+        building.cohorts.append(new_cohort)
+    
+
+def create_native():
+    number_of_natives=int(input("how many natives do you want to create?\n"))
+    for _ in range(number_of_natives):
+        first_name = input("Enter native first name\n")
+        last_name = input("Enter native last name\n")
+        sex = input("Enter native gender\n")
+        native_id= input("Enter native id\n")
+        new_native = class_task.Native(first_name, last_name, sex, native_id)
+        building.cohorts.add_native_to_cohort(new_native)
+
+def display_building():
+    for cohort in building.cohorts:
+        print (cohort.name+":")
+        print("SCN  No.|   First name   |   Last name   |   sex")
+        for native in cohort.natives:
+            print(native.native_id +   "        |"     + native.first_name + "          |"  + native.last_name  + "         |" + native.sex+"\n")
+        print("="*105 + "\n")
+
 if __name__=="__main__":
     response=input("Do you want to create a building?\n")
     if response == "yes":
@@ -11,51 +39,27 @@ if __name__=="__main__":
                 2-> create native
                 3-> exit
         """))
+  
         if response == 1:
-            number_of_cohorts=int(input("how many cohorts do you want to create?"))
-            for cohort in range(number_of_cohorts):
-                cohort_name = input("Enter cohort name\n")
-                cohort_desc = input("Enter cohort description\n")
-                new_cohort = class_task.Cohort(cohort_name, cohort_desc)
-                building.cohorts.append(new_cohort)
-            
+            create_cohort()
+            response=input("create natives?")   
+            if response=="yes":
+                create_native() 
+            else:
+                display_building()   
         elif response == 2:
-            number_of_natives=int(input("how many cohorts do you want to create?"))
-            for native in range(number_of_natives):
-                first_name = input("Enter native first name\n")
-                last_name = input("Enter native last name\n")
-                sex = input("Enter native gender\n")
-                native_id= input("Enter native id\n")
+            create_native()
+    display_building()
 
-                new_native = class_task.Native(first_name, last_name, sex, native_id)
-                building.cohorts.add_native_to_cohort(new_native)
+
         
-        for cohort in building.cohorts:
-            print (cohort.name+":")
-            print("SCN  No.|   First name   |   Last name   |   sex")
-            for native in cohort.natives:
-                print(native.native_id +   "        |"     + native.first_name + "          |"  + native.last_name  + "         |" + native.sex+"\n")
-            print("="*105 + "\n")
 
-            # elif cohort.name=="cohort_two":
-            #     print (cohort.name+":")
-            #     print("SCN  No.|   First name   |   Last name   |   sex")
-            #     for native in cohort.natives:
-            #         print(native.native_id +   "        |"     + native.first_name + "          |"  + native.last_name  + "         |" + native.sex)
 
-            #     print("="*105 + "\n")
-
-            # elif cohort.name == "cohort_three":
-            #     print (cohort.name+":")
-            #     print("SCN  No.|   First name   |   Last name   |   sex")
-            #     for native in cohort.natives:
-            #         print(native.native_id +   "        |"     + native.first_name + "          |"  + native.last_name  + "         |" + native.sex)
-            #     print("="*105 + "\n")
-            # else:
-            #     print(cohort.name+":")
-            #     print("SCN  No.|   First name   |   Last name   |   sex")
-            #     for native in cohort.natives:
-            #         print(native.native_id +   "        |"     + native.first_name + "          |"  + native.last_name  + "         |" + native.sex)
-            #     print("=" * 105)
+       
+            
+       
+            
+        
+     
 
     
