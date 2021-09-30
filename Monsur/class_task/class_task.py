2391c87b8d43276543525c68e11ffd0deb16d78b
Monsur Kareem <monsurkareem@gmail.com>
2021-09-30 12:17:37 +0100

    new class work- edit 2

--- Monsur/class_task/class_task.py
+++ Monsur/class_task/class_task.py
@@ -1,4 +1,4 @@
-class Building(object):
+class Building:
     def __init__(self, name, cohorts):
         self.name = name
         self.cohorts = cohorts
@@ -7,7 +7,7 @@ class Building(object):
         return self.name
 
 
-class Cohort(object):
+class Cohort:
     def __init__(self, name, description, natives):
         self.name = name
         self.description = description
@@ -17,28 +17,28 @@ class Cohort(object):
         return self.name
 
 
-class Native(object):
+def _native_first_name(first_name):
+    if len(first_name) > 25:
+        raise ValueError("First name cannot exceed 25 characters, you know what to abi?")
+    return first_name
+
+
+class Native:
     def __init__(self, first_name, last_name, sex, native_id):
         self.first_name = first_name
         self.last_name = last_name
         self.sex = sex
         self.native_id = native_id
 
-    def _native_first_name (self, first_name):
-        if len(first_name) > 25:
-            raise ValueError("first name cannot exceed 25 characters")
-        return first_name
-
-    def _native_last_name (self, last_name):
+def _native_last_name(self, last_name):
         if len(last_name) > 25:
             raise ValueError("last name cannot exceed 25 characters")
         return last_name
 
-    def _native_sex(self, sex):
+def _native_sex(self, sex):
         if sex != "male" and sex != "female":
             raise ValueError("sex must either be male or female")
         return sex
-
 
     def __str__(self) -> str:
         return self.first_name + " " + self.last_name
